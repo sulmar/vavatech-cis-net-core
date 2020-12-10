adbdaa5c4b9a01b266fe3515385c49edaa559275
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-12-10 16:26:48 +0100

    Topshelf

---
+++ src/Vavatech.CIS.FileService/FileSystemWatcherService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Topshelf;
+
+namespace Vavatech.CIS.FileService
+{
+    // dotnet add package Microsoft.Extensions.Logging
+
+    public class FileSystemWatcherService : ServiceControl
+    {
+        private readonly FileSystemWatcher fileSystemWatcher = new FileSystemWatcher();
+
+        public FileSystemWatcherService(string directory)
+        {
+            // string directory = @"C:\temp\CIS";
+
+            fileSystemWatcher.Path = directory;
+            fileSystemWatcher.Filters.Add("*.txt");
+            fileSystemWatcher.Filters.Add("*.bmp");
+
+            fileSystemWatcher.Created += FileSystemWatcher_Created;
+            fileSystemWatcher.Changed += FileSystemWatcher_Changed;
+            fileSystemWatcher.Renamed += FileSystemWatcher_Renamed;
+            fileSystemWatcher.Deleted += FileSystemWatcher_Deleted;
+
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            fileSystemWatcher.EnableRaisingEvents = true;
+
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            fileSystemWatcher.EnableRaisingEvents = false;
+
+            return true;
+        }
+
+        private void FileSystemWatcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"{e.ChangeType} {e.Name}");
+        }
+
+        private void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{e.ChangeType} {e.Name}");
+        }
+
+        private void FileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{e.ChangeType} {e.Name}");
+        }
+
+
+        private void FileSystemWatcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{e.ChangeType} {e.Name}");
+        }
+    }
+}
--- src/Vavatech.CIS.FileService/Program.cs
+++ src/Vavatech.CIS.FileService/Program.cs
@@ -1,76 +1,30 @@
 ﻿using System;
 using System.IO;
+using Topshelf;
 
 namespace Vavatech.CIS.FileService
 {
+    // dotnet add package Topshelf
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.ResetColor();
-
-            Console.WriteLine("Hello File Service!");
-
             string directory = @"C:\temp\CIS";
 
-            MonitorDirectory(directory);
+            HostFactory.Run(x =>
+            {
+                x.Service<FileSystemWatcherService>(service =>
+                {
+                    service.ConstructUsing(s => new FileSystemWatcherService(directory));
+                });
 
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadLine();
+                x.SetServiceName("CISService");
+                x.SetDescription("CIS Service using Topshelf");
+                x.StartAutomatically();
+            });
 
+            // Vavatech.CIS.FileService.exe install start
         }
-
-        private static void MonitorDirectory(string directory)
-        {
-            FileSystemWatcher fileSystemWatcher = new FileSystemWatcher();
-            fileSystemWatcher.Path = directory;
-            // fileSystemWatcher.Filter = "*.txt";
-
-            fileSystemWatcher.Filters.Add("*.txt");
-            fileSystemWatcher.Filters.Add("*.bmp");
-
-            fileSystemWatcher.Created += FileSystemWatcher_Created;
-            fileSystemWatcher.Changed += FileSystemWatcher_Changed;
-            fileSystemWatcher.Renamed += FileSystemWatcher_Renamed;
-            fileSystemWatcher.Deleted += FileSystemWatcher_Deleted;
-
-            // Włączyć !!!
-            fileSystemWatcher.EnableRaisingEvents = true;
-
-        }
-
-        private static void FileSystemWatcher_Renamed(object sender, RenamedEventArgs e)
-        {
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-            Console.WriteLine($"{e.ChangeType} {e.Name}");
-            Console.ResetColor();
-        }
-
-        private static void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"{e.ChangeType} {e.Name}");
-            Console.ResetColor();
-        }
-
-        private static void FileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
-        {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine($"{e.ChangeType} {e.Name}");
-            Console.ResetColor();
-        }
-
-
-        private static void FileSystemWatcher_Deleted(object sender, FileSystemEventArgs e)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"{e.ChangeType} {e.Name}");
-            Console.ResetColor();
-        }
-
-     
-       
-
-
     }
 }
